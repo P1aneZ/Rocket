4acb6437cd194f3ad7d2a50583d1a741d40fe053
Berluga <149092042+Berluga@users.noreply.github.com>
2024-01-18 13:28:12 +0800

    给予墙面与地面物理材质

--- Assets/Scripts/RocketMove.cs
+++ Assets/Scripts/RocketMove.cs
@@ -28,18 +28,22 @@ public class RocketMove : MonoBehaviour
 
     private void FollowMouseRotate()//用来让Rocket跟随鼠标旋转
     {
-            //获取鼠标位置，鼠标坐标是屏幕坐标,Z轴为0，这里不做转换 
-            Vector3 mouse = Input.mousePosition;
-            //获取火箭坐标，火箭坐标是世界坐标，需要转换为屏幕坐标
-            Vector3 obj = Camera.main.WorldToScreenPoint(transform.position);
-            //屏幕坐标向量相减，得到鼠标和火箭之间的连线向量
-            direction = mouse - obj;
-            //将z轴赋值为0，置于平面空间内
-            direction.z = 0f;
-            //将目标向量长度变为1，这里只需要向量方向，不需要长度，所以变成1.
-            direction = direction.normalized;
-            //物体自身的y轴与目标向量一致，达到旋转效果
-            transform.up = direction;        
+
+
+        //获取鼠标位置，鼠标坐标是屏幕坐标,Z轴为0，这里不做转换 
+        Vector3 mouse = Input.mousePosition;
+        //获取火箭坐标，火箭坐标是世界坐标，需要转换为屏幕坐标
+        Vector3 obj = Camera.main.WorldToScreenPoint(transform.position);
+        //屏幕坐标向量相减，得到鼠标和火箭之间的连线向量
+        direction = mouse - obj;
+        //将z轴赋值为0，置于平面空间内
+        direction.z = 0f;
+        //将目标向量长度变为1，这里只需要向量方向，不需要长度，所以变成1.
+        direction = direction.normalized;
+        //物体自身的y轴与目标向量一致，达到旋转效果
+        if (Input.GetMouseButton(0))
+            transform.up = direction;
+
     }
 
     private void FollowMouseMove()
