607e403170b807c5f30246da793fbf06d5e16592
P1aneZ <148950245+P1aneZ@users.noreply.github.com>
2024-01-18 19:54:50 +0800

    摧毁、补给装置的添加&着陆装置判定优化
    补给和着陆的判定已改为火箭在相应区域中停留3s才判定成功
    摧毁装置暂时只做了火箭碰撞就判断摧毁的机制

--- Assets/Scripts/PhysicsCheck.cs
+++ Assets/Scripts/PhysicsCheck.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
-    public bool isLand;//着陆与否
+    [Header("判断是否碰撞")]
+    public bool isWithLand;//进入着陆装置判定区域与否
 
-    public LayerMask LandPointLayer;//着陆点所在的层
+    public bool isWithDestroyDevice;
 
+    public bool isWithSupplyDevice;
+
+    [Header("判断碰撞的Layer")]
+    public LayerMask LandPointLayer;//着陆装置所在的层
+    public LayerMask DestroyDeviceLayer;
+    public LayerMask SupplyDeviceLayer;
+    [Header("判断碰撞的半径")]
     public float checkRadius=0.1f;//判定范围大小
 
     // Update is called once per frame
     void Update()
     {
-        CheckLand();//一直运行CheckLand函数，判断是否着陆
+        CheckLand();//一直运行CheckLand函数，判断是否进入着陆装置判定区域
+        CheckDestroy();
+        CheckSupply();
     }
     private void CheckLand()
     {
-        isLand=Physics2D.OverlapCircle(transform.position,checkRadius,LandPointLayer);//以transform.position为中心，checkRadius为半径的圆的范围内，在LandPointLayer上检测是否有碰撞，如果有，isLand=true
+        isWithLand=Physics2D.OverlapCircle(transform.position,checkRadius,LandPointLayer);//以transform.position为中心，checkRadius为半径的圆的范围内，在LandPointLayer上检测是否有碰撞，如果有，isLand=true
+    }
+
+    public void CheckDestroy()
+    {
+        isWithDestroyDevice=Physics2D.OverlapCircle(transform.position,checkRadius,DestroyDeviceLayer);
+    }
+
+    private void CheckSupply()
+    {
+        isWithSupplyDevice = Physics2D.OverlapCircle(transform.position,checkRadius,SupplyDeviceLayer);
     }
 }
