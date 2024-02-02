776dca4e99ef4501fa817841951bed50c0265043
Berluga <149092042+Berluga@users.noreply.github.com>
2024-02-02 16:30:42 +0800

    摧毁装置动画

--- Assets/Scripts/Device/DestoryDeviceAnimation.cs
+++ Assets/Scripts/Device/DestoryDeviceAnimation.cs
@@ -6,13 +6,31 @@ public class DestoryDeviceAnimation : MonoBehaviour
 {    
     public Animator anim;
 
+    private Character character;
+
     public void Awake()
     {
         anim = GetComponent<Animator>();
+        character = GetComponent<Character>();
+    }
+
+    void Update()
+    {
+        SetAnimation();
+    }
+
+    public void SetAnimation()
+    {
+        anim.SetBool("isDead", character.isDead);
     }
 
     public void DeviceHurt()
     {
         anim.SetTrigger("hurt");
+    }
+
+    public void DestoryAfterDead()
+    {
+        Destroy(this.gameObject);
     }
 }
