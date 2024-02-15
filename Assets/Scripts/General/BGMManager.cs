6a33b764794aada643b44b737781c8b88de21d54
Berluga <149092042+Berluga@users.noreply.github.com>
2024-02-15 19:17:34 +0800

    主题曲放主界面，氛围曲放在进关卡之后的游戏内

--- Assets/Scripts/General/BGMManager.cs
+++ Assets/Scripts/General/BGMManager.cs
@@ -2,12 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BGMManager : MonoBehaviour
-{       
+{
+    public AudioSource audioSource;
+    public AudioClip BGM01;
+    public AudioClip BGM02;
+
+    public bool isInMenu = true;
+
+    public string sceneName;
+
     // Start is called before the first frame update
     void Start()
-    {       
+    {
         DontDestroyOnLoad(this.gameObject);
-    }   
+    }
+
+    private void Update()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneName == "Menu" && !isInMenu)
+        {
+            isInMenu = true;
+            AtMenu();
+        }
+        else if (sceneName == "level1" || sceneName == "level2" || sceneName == "Level3" || sceneName == "Level4" || sceneName == "Level5"
+            || sceneName == "Level6" || sceneName == "Level7" || sceneName == "Level8" || sceneName == "Level9")
+        {
+            if (isInMenu)
+            {
+                isInMenu = false;
+                AtLevel();
+            }
+        }
+    }
+
+
+    private void AtMenu()
+    {
+        audioSource.Stop();
+        audioSource.clip = BGM01;
+        audioSource.Play();
+    }
+
+    private void AtLevel()
+    {
+        audioSource.Stop();
+        audioSource.clip = BGM02;
+        audioSource.Play();
+    }
 }
